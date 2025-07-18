26f8d6c39e2a6a802aea1a40fafe3c76923df549
Ubuntu <ubuntu@ip-172-31-34-197.eu-west-2.compute.internal>
2025-07-18 11:02:58 +0000

    cleanup logic changes

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
-docker rm -f $(docker ps -q)
-docker rmi $(docker images -q)
-docker volume rm -f $(docker volume ls -q)
+# Remove running containers if any
+containers=$(docker ps -q)
+if [ -n "$containers" ]; then
+  docker rm -f $containers
+else
+  echo "No running containers to remove."
+fi
+
+# Remove images if any
+images=$(docker images -q)
+if [ -n "$images" ]; then
+  docker rmi -f $images
+else
+  echo "No images to remove."
+fi
+
+# Remove volumes if any
+volumes=$(docker volume ls -q)
+if [ -n "$volumes" ]; then
+  docker volume rm -f $volumes
+else
+  echo "No volumes to remove."
+fi
